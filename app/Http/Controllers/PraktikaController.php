4b5b0bccc9a58c84435b298e7f3303a235de4987
t-o-m-o <37299796+t-o-m-o@users.noreply.github.com>
2018-05-28 14:35:36 +0200

    Add files via upload

--- app/Http/Controllers/PraktikaController.php
+++ app/Http/Controllers/PraktikaController.php
@@ -12,7 +12,7 @@ class Praktikacontroller extends Controller
 
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use App\user;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class Usercontroller extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +19,8 @@ class Usercontroller extends Controller
      */
     public function index()
     {
-        //
+        $users = user::paginate(25);
+        return view('user.index', compact('users'));
     }
 
     /**
@@ -46,7 +52,7 @@ class Usercontroller extends Controller
      */
     public function show(user $user)
     {
-        //
+        return view('user.show', compact('user'));
     }
 
     /**
@@ -57,7 +63,7 @@ class Usercontroller extends Controller
      */
     public function edit(user $user)
     {
-        //
+        return view('user.edit', compact('user'));
     }
 
     /**
@@ -69,7 +75,26 @@ class Usercontroller extends Controller
      */
     public function update(Request $request, user $user)
     {
-        //
+        $request->validate([
+          'name' => 'required',
+          'email' => 'email',
+          'typ' => ['required',Rule::in(
+            ['admin', 'user']
+          ),]
+      ]);
+        $user->update(array(
+          'name' => request('name'),
+          'email' => request('email'),
+          'typ' => request('typ')
+      ));
+
+        try {
+            $user->save();
+        } catch (\Exception $e) {
+            return view('user.edit', $user)->withErrors($e->getMessage());
+        }
+
+        return redirect(route('user.show', $user));
     }
 
     /**
@@ -80,6 +105,11 @@ class Usercontroller extends Controller
      */
     public function destroy(user $user)
     {
-        //
+        try {
+            $user->delete();
+        } catch (\Exception $e) {
+            return view('user.show', compact('user'))->withErrors($e->getMessage());
+        }
+        return $this->index();
     }
 }
